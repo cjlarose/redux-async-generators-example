3f30160697266731e89b515036309b139692f458
Chris LaRose <cjlarose@gmail.com>
2016-11-18 16:54:17 -0800

    Use babel as eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "browser": true
   },
   "extends": "airbnb",
+  "parser": "babel-eslint",
   "plugins": ["import"],
   "rules": {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
